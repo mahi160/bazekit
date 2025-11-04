91f96a7461d308f78ac0d74d0610e7f070c2c8a2
mahi160 <omarsifat288@gamil.com>
2025-11-04 19:47:27 +0600

    feat(switch): add color and size

--- react/src/components/switch/Switch.tsx
+++ react/src/components/switch/Switch.tsx
@@ -1,20 +1,26 @@
 import { Switch as BaseSwitch } from "@base-ui-components/react/switch";
 import styles from "./Switch.module.css";
+import * as React from "react";
 
 export interface ISwitchProps extends BaseSwitch.Root.Props {
   color?: "brand" | "accent" | "alert";
   size?: "sm" | "md" | "lg";
 }
-export const Switch: React.FC<ISwitchProps> = (props) => {
-  const { className, color, size, ...rest } = props;
-  return (
-    <BaseSwitch.Root
-      data-color={color}
-      data-size={size}
-      className={styles.switch}
-      {...rest}
-    >
-      <BaseSwitch.Thumb className={styles.thumb}></BaseSwitch.Thumb>
-    </BaseSwitch.Root>
-  );
-};
+
+export const Switch = React.forwardRef<HTMLButtonElement, ISwitchProps>(
+  ({ className = "", color, size, ...rest }, ref) => {
+    return (
+      <BaseSwitch.Root
+        ref={ref}
+        data-color={color}
+        data-size={size}
+        className={`${styles.switch} ${className}`.trim()}
+        {...rest}
+      >
+        <BaseSwitch.Thumb className={styles.thumb} />
+      </BaseSwitch.Root>
+    );
+  }
+);
+
+Switch.displayName = "Switch";
